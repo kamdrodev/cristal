4539d1be3c012a738e3db3ec42fc679c77042014
kamdrodev <kamdrodev@protonmail.com>
2020-01-24 05:27:50 +0100

    crud lists

--- client/src/components/Lists.vue
+++ client/src/components/Lists.vue
@@ -7,51 +7,37 @@
 
 
     <q-list bordered class="rounded-borders" v-for='list in lists'>
-      <q-item-label header>Lists</q-item-label>
-
+      <q-item-label header>{{list.title}}</q-item-label>
       <q-item>
         <q-item-section avatar top>
-          <q-icon name="account_tree" color="black" size="34px" />
+          <q-icon name="list" size="34px" />
         </q-item-section>
 
         <q-item-section top class="col-2 gt-sm">
-          <q-item-label class="q-mt-sm">GitHub</q-item-label>
+          <q-item-label class="q-mt-sm">{{list.firstLanguage.substring(0,3)}} <-> {{list.secondLanguage.substring(0,3)}}</q-item-label>
         </q-item-section>
 
         <q-item-section top>
           <q-item-label lines="1">
-            <span class="text-weight-medium">[quasarframework/quasar]</span>
-            <span class="text-grey-8"> - GitHub repository</span>
-          </q-item-label>
-          <q-item-label caption lines="1">
-            @rstoenescu in #3: > Generic type parameter for props
-          </q-item-label>
-          <q-item-label lines="1" class="q-mt-xs text-body2 text-weight-bold text-primary text-uppercase">
-            <span class="cursor-pointer">Open in GitHub</span>
+            <span class="text-weight-medium">{{list.description}}</span>
+            <span class="text-grey-8"></span>
           </q-item-label>
         </q-item-section>
-
         <q-item-section top side>
           <div class="text-grey-8 q-gutter-xs">
-            <q-btn class="gt-xs" size="12px" flat dense round icon="edit" />
-            <q-btn class="gt-xs" size="12px" flat dense round icon="delete" />
-            <q-btn size="12px" flat dense round icon="more_vert" />
+            <q-btn class="gt-xs" size="12px" flat dense round icon="edit" @click="openPromptUpdateList(list._id, list.title, list.description)" />
+            <q-btn class="gt-xs" size="12px" flat dense round icon="delete" @click="openPromptDeleteList(list._id)" />
+            <q-btn size="12px" flat dense round icon="dashboard" />
           </div>
         </q-item-section>
-      </q-item>
-
-
-
-      
-
-     
+      </q-item>     
     </q-list>
 
 
 
 
       <q-page-sticky position="bottom-right" :offset="[18, 18]">
-        <q-btn fab icon="post_add" @click="promptCreateList = true" />
+        <q-btn fab icon="post_add" @click="openPromptCreateList" />
       </q-page-sticky>
     </div>
 
@@ -95,6 +81,52 @@
       </q-card>
     </q-dialog>
 
+   
+    <!-- Dialog - Update List -->
+    <q-dialog v-model="promptUpdateList" persistent>
+      <q-card style="min-width: 350px">
+        <form c>
+        <q-card-section>
+          <div class="text-h6">Update list</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <!-- <q-input dense v-model="address" autofocus @keyup.enter="promptCreateList = false" /> -->
+          <q-input filled v-model="formUpdateList.title" label="Title" :error="$v.formUpdateList.title.$error" class="q-pb-lg">
+            <template v-slot:prepend>
+              <q-icon name="mail" />
+            </template>
+          </q-input>
+          <q-input filled v-model="formUpdateList.description" label="Description" :error="$v.formUpdateList.description.$error" class="q-pb-lg">
+            <template v-slot:prepend>
+              <q-icon name="mail" />
+            </template>
+          </q-input>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Create list" @click="updateList" />
+        </q-card-actions>
+      </form>
+      </q-card>
+    </q-dialog>
+
+     <!-- Dialog - Delete list -->
+     <q-dialog v-model="promptDeleteList" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
+          <span class="q-ml-sm">You are currently not connected to any network.</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Turn on Wifi" color="primary" @click="deleteList" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
   </div> 
 </template>
 
@@ -105,6 +137,8 @@ export default {
   name: 'Lists',
   data: () => ({
     promptCreateList: false,
+    promptUpdateList: false,
+    promptDeleteList: false,
     listsQueryOptions: {
       secondLanguage: 'french',
     },
@@ -118,11 +152,19 @@ export default {
       'japanese',
     ],
     formCreateList: {
-      title: "",
-      description: "",
-      firstLanguage: "polish",
-      secondLanguage: "french"
-    }
+      title: '',
+      description: '',
+      firstLanguage: 'polish',
+      secondLanguage: 'french',
+    },
+    formUpdateList: {
+      id: '',
+      title: '',
+      description: '',
+    },
+    formDeletelist: {
+      id: '',
+    },
   }),
   validations: {
     formCreateList: {
@@ -138,6 +180,14 @@ export default {
       description: {
         required,
       }
+    },
+    formUpdateList: {
+      title: {
+        required,
+      },
+      description: {
+        required,
+      },
     }
   },
   async created() {
@@ -153,6 +203,35 @@ export default {
     }
   },
   methods: {
+    openPromptCreateList() {
+      this.promptCreateList = true
+    },
+    openPromptUpdateList(id, title, description) {
+      this.promptUpdateList = true
+      this.formUpdateList = {
+        id,
+        title,
+        description,
+      }
+    },
+    openPromptDeleteList(id) {
+      this.promptDeleteList = true
+      this.formDeleteList = {
+        id,
+      }
+    },
+    async getAllLists() {
+      try {
+      const getAllListsProcess = await this.$store.dispatch('lists/getAllLists', {
+          listsQueryOptions: this.listsQueryOptions,
+        })
+
+        this.$q.notify({message: getAllListsProcess.message, color: 'positive'})
+      } catch (e) {
+        console.log(e)
+        this.$q.notify({message: e.message, color: 'negative'})
+      }
+    },
     async createList() {
       try {
         console.log('Create list')
@@ -169,7 +248,45 @@ export default {
           description: this.formCreateList.description,
         })
 
+        await this.getAllLists()
+
         this.$q.notify({message: createListProcess.message, color: 'positive'})
+      } catch (e) {
+        this.$q.notify({message: e.message, color: 'negative'})
+      }
+    },
+    async updateList() {
+      try {
+        console.log('Edit list')
+        this.$v.formUpdateList.$touch()
+
+        if (this.$v.formUpdateList.$error) {
+          throw new Error('Please review fields again.')
+        }
+        
+        const updateListProfcess = await this.$store.dispatch('lists/updateList', {
+          id: this.formUpdateList.id,
+          title: this.formUpdateList.title,
+          description: this.formUpdateList.description,
+        })
+
+        await this.getAllLists()
+
+        this.$q.notify({message: updateListProfcess.message, color: 'positive'})
+      } catch (e) {
+        this.$q.notify({message: e.message, color: 'negative'})
+      }
+    },
+    async deleteList() {
+      try {
+        
+        const deleteListProcess = await this.$store.dispatch('lists/deleteList', {
+          id: this.formDeleteList.id
+        })
+
+        await this.getAllLists()
+
+        this.$q.notify({message: deleteListProcess.message, color: 'positive'})
       } catch (e) {
         this.$q.notify({message: e.message, color: 'negative'})
       }
